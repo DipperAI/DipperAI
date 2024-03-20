4f9dfe553bfef2ce811560b83e661bd25d8bb574
anycodes <liuyu@xmail.tech>
2024-03-20 16:17:45 +0800

    update mass-ms

--- maas/modelscope.py
+++ maas/modelscope.py
@@ -1,3 +1,5 @@
+import json
+import requests
 from maas.core import MaaS
 from version import __version__
 
@@ -32,4 +34,25 @@ class Modelscope(MaaS):
         }
         self.token = None
 
-    # Rest of the code...
+    def login(self):
+        """
+        login to modelscope, get cookie and add it to self.headers;
+        source code: https://modelscope.cn/docs/api_docs/API文档%2Fbuild%2Fjson%2F_modules%2Fmodelscope%2Fhub%2Fapi%2F%23HubApi.login
+        :return:
+        """
+        login_url = "https://modelscope.cn/api/v1/login"
+        payload = json.dumps({"AccessToken": self.token})
+        response_attr = requests.request("POST", login_url, headers=self.headers, data=payload)
+        self.headers["Cookie"] = response_attr.cookies
+
+    def get_task(self):
+        """
+        get modelscope model task type, automatically generated in file configuration.json;
+        source code: https://modelscope.cn/docs/api_docs/API文档%2Fbuild%2Fjson%2F_modules%2Fmodelscope%2Fhub%2Ffile_download%2F%23model_file_download
+        :return: Task type (https://modelscope.cn/docs/模型的推理Pipeline#当前支持的task列表)
+        """
+        file_url = 'https://modelscope.cn/api/v1/models/%s/repo?Revision=%s&FilePath=configuration.json' % (
+            self.model_id, self.model_version)
+        if self.token: self.login()
+        response_json = json.loads(requests.request("POST", file_url, headers=self.headers).content.decode("utf-8"))
+        return response_json["task"]
